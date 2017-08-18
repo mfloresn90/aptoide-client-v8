5c2443f8d697d06a2d9ac356cec16387bb1fcbd1
diogoloureiro <!a12110650>
2017-08-18 15:13:55 +0100

    [AO-285] added storeTheme and storeName arguments to the AppBrickListWidget on open AppViewFragment

--- app/src/main/java/cm/aptoide/pt/view/store/featured/AppBrickWidget.java
+++ app/src/main/java/cm/aptoide/pt/view/store/featured/AppBrickWidget.java
@@ -34,20 +34,16 @@ public class AppBrickWidget extends Widget<AppBrickDisplayable> {
   @Override public void bindView(AppBrickDisplayable displayable) {
     final FragmentActivity context = getContext();
     ImageLoader.with(context)
-        .load(displayable.getPojo()
-            .getGraphic(), R.drawable.placeholder_brick, graphic);
+        .load(displayable.getPojo().getGraphic(), R.drawable.placeholder_brick, graphic);
 
-    compositeSubscription.add(RxView.clicks(itemView)
-        .subscribe(v -> {
-          Analytics.AppViewViewedFrom.addStepToList(displayable.getTag());
-          getFragmentNavigator().navigateTo(V8Engine.getFragmentProvider()
-              .newAppViewFragment(displayable.getPojo()
-                  .getId(), displayable.getPojo()
-                  .getPackageName()));
-          Analytics.HomePageEditorsChoice.clickOnEditorsChoiceItem(getAdapterPosition(),
-              displayable.getPojo()
-                  .getPackageName(), true);
-        }, throwable -> CrashReport.getInstance()
-            .log(throwable)));
+    compositeSubscription.add(RxView.clicks(itemView).subscribe(v -> {
+      Analytics.AppViewViewedFrom.addStepToList(displayable.getTag());
+      getFragmentNavigator().navigateTo(V8Engine.getFragmentProvider()
+          .newAppViewFragment(displayable.getPojo().getId(), displayable.getPojo().getPackageName(),
+              displayable.getPojo().getStore().getAppearance().getTheme(),
+              displayable.getPojo().getStore().getName()));
+      Analytics.HomePageEditorsChoice.clickOnEditorsChoiceItem(getAdapterPosition(),
+          displayable.getPojo().getPackageName(), true);
+    }, throwable -> CrashReport.getInstance().log(throwable)));
   }
 }

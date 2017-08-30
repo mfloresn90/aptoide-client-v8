7b182ba95421154614095cb6dc7f95d23892145d
diogoloureiro <!a12110650>
2017-08-30 10:26:38 +0100

    [AO-285] moved UIComponentFragment to vanilla dimension

--- app/src/vanilla/java/cm/aptoide/pt/view/fragment/UIComponentFragment.java
+++ app/src/vanilla/java/cm/aptoide/pt/view/fragment/UIComponentFragment.java
@@ -6,9 +6,6 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import cm.aptoide.pt.preferences.Application;
-import cm.aptoide.pt.store.StoreTheme;
-import cm.aptoide.pt.view.ThemeUtils;
 import cm.aptoide.pt.view.permission.PermissionServiceFragment;
 
 /**
@@ -51,11 +48,6 @@ public abstract class UIComponentFragment extends PermissionServiceFragment impl
   @CallSuper @Nullable @Override
   public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
       @Nullable Bundle savedInstanceState) {
-    String storeTheme = Application.getConfiguration().getDefaultTheme();
-    if (storeTheme != null) {
-      ThemeUtils.setStoreTheme(getActivity(), storeTheme);
-      ThemeUtils.setStatusBarThemeColor(getActivity(), StoreTheme.get(storeTheme));
-    }
     super.onCreateView(inflater, container, savedInstanceState);
     return inflater.inflate(getContentViewId(), container, false);
   }
